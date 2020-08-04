b8ba13a2e384093a2e2874c520f0c64ac1fbb5b5
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-08-04 13:35:53 -0400

    Updates

---
+++ component---src-pages-contact-js-2d9a866c0a507e853906.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Wbzz"),r=n("Bl7J"),o=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(o.a,{title:"Contact"}),l.a.createElement("h1",null,"Contact"),l.a.createElement("p",null,"COMING SOON!"),l.a.createElement("br",null),l.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-2d9a866c0a507e853906.js.map
