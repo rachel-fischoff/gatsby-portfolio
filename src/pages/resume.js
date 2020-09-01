3a408ce18f28c0a0a39be0ad7a59743bfe47bcbf
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-09-01 15:43:45 -0400

    with resumes linking to github for pdf

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -8,9 +8,19 @@ const Resume = () => (
   <Layout>
     <SEO title="Resume" />
     <h1>Resume</h1>
-    <p>COMING SOON! </p>
-    
-    <br/>
+
+    <a href="https://github.com/rachel-fischoff/gatsby-portfolio/blob/master/src/images/Rachel_Fischoff_FE_SEPT_2020.pdf">
+      Front End Resume
+    </a>
+    <br />
+    <br />
+    <a href="https://github.com/rachel-fischoff/gatsby-portfolio/blob/master/src/images/Rachel_Fischoff_FS_Sept_2020.pdf">
+      Full Stack Resume
+    </a>
+
+    <br />
+    <br />
+    <br />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
